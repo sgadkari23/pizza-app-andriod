c3beb845807995e08bad99a5fe3e6e754411a44b
Supriya G <sgadkar1@my.centennialcollege.ca>
2020-10-15 10:01:08 -0500

    Intent Functionality from main activity

--- app/src/main/java/com/example/pizzaorderingapp/CheckoutActivity.kt
+++ app/src/main/java/com/example/pizzaorderingapp/CheckoutActivity.kt
@@ -4,15 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class CheckoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
+
+       // val intent = intent
+       // val message = intent.getStringExtra("PizzaMenueType")
+        val textView = findViewById<TextView>(R.id.textView8)
+        textView.text = intent.getStringExtra("PizzaMenueType")
+
+       /* val textView1 = findViewById<TextView>(R.id.textView9)
+        textView.text = message
+
+        val textView3 = findViewById<TextView>(R.id.textView8)
+        textView.text = message*/
+
     }
 
     // Method to handle show button on click
-    fun handleOnConfirmButtonClick(v: View) {
+    /*fun handleOnConfirmButtonClick(v: View) {
         if (v.id == R.id.btnConfirmation) {
 
             val intent = Intent(this@CheckoutActivity, OrderDetailActivity::class.java)
@@ -20,5 +33,5 @@ class CheckoutActivity : AppCompatActivity() {
 
 
         }
-    }
+    } */
 }
--- app/src/main/java/com/example/pizzaorderingapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderingapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.MenuItem
 import android.widget.EditText
 import android.widget.Toast
 import com.example.pizzaorderingapp.R.id.meatSupreme
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,35 +21,44 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    class order:Serializable{
+        
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             meatSupreme ->{
-            val intent = Intent(this@MainActivity, PizzaSizeActivity::class.java)
-                val mnuMeatSupreme: String = getString(R.string.meat_supreme)
-                intent.putExtra("MeatSupreme", mnuMeatSupreme)
+            val intent = Intent(this@MainActivity, CheckoutActivity::class.java)
+              //  val mnuMeatSupreme: String = getString(R.string.meat_supreme)
+              //  val pizzaType: String = getString(R.string.meat_supreme)
+                intent.putExtra("PizzaMenueType", getString(R.string.meat_supreme))
                 Toast.makeText(this, "You clicked Meat Supreme", Toast.LENGTH_LONG).show()
                 startActivity(intent)
             }
             R.id.superHawaiian ->{
-                val intent = Intent(this@MainActivity, PizzaSizeActivity::class.java)
+                val intent = Intent(this@MainActivity, CheckoutActivity::class.java)
+                intent.putExtra("PizzaMenueType", getString(R.string.super_hawaiian))
                 startActivity(intent)
                 Toast.makeText(this, "You clicked Super Hawaiian", Toast.LENGTH_LONG).show()
             }
 
             R.id.veggie ->{
-                val intent = Intent(this@MainActivity, PizzaSizeActivity::class.java)
+                val intent = Intent(this@MainActivity, CheckoutActivity::class.java)
+                intent.putExtra("PizzaMenueType", getString(R.string.veggie))
                 startActivity(intent)
                 Toast.makeText(this, "You clicked Veggie", Toast.LENGTH_LONG).show()
             }
 
             R.id.mediterranean ->{
-                val intent = Intent(this@MainActivity, PizzaSizeActivity::class.java)
+                val intent = Intent(this@MainActivity, CheckoutActivity::class.java)
+                intent.putExtra("PizzaMenueType", getString(R.string.mediterranean))
                 startActivity(intent)
                 Toast.makeText(this, "You clicked Mediterranean", Toast.LENGTH_LONG).show()
             }
 
             R.id.cheddarSupreme ->{
-                val intent = Intent(this@MainActivity, PizzaSizeActivity::class.java)
+                val intent = Intent(this@MainActivity, CheckoutActivity::class.java)
+                intent.putExtra("PizzaMenueType", getString(R.string.cheddar_supreme))
                 startActivity(intent)
                 Toast.makeText(this, "You clicked Cheddar Supreme", Toast.LENGTH_LONG).show()
             }
